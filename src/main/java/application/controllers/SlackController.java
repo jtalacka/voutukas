38009df3348f87948c0385a064be8380089dc732
Karolis Medekša <kreisis4@gmail.com>
2019-10-31 13:03:06 +0200

    Returning modal update

--- src/main/java/application/controllers/SlackController.java
+++ src/main/java/application/controllers/SlackController.java
@@ -1,7 +1,12 @@
 package application.controllers;
 
 import application.business.SlackManager;
+import com.github.seratch.jslack.api.model.view.View;
+import com.github.seratch.jslack.api.model.view.ViewClose;
+import com.github.seratch.jslack.api.model.view.ViewSubmit;
+import com.github.seratch.jslack.api.model.view.ViewTitle;
 import com.github.seratch.jslack.app_backend.views.payload.ViewSubmissionPayload;
+import com.github.seratch.jslack.app_backend.views.response.ViewSubmissionResponse;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,9 +27,21 @@ public class SlackController {
     }
 
     @RequestMapping(value = "/slack/interact", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public void onInteraction(@RequestParam("payload") String jsonResponse){
-        ViewSubmissionPayload payload = GsonFactory.createSnakeCase().fromJson(jsonResponse, ViewSubmissionPayload.class);
-        System.out.println(payload);
+    public String onInteraction(@RequestParam("payload") String jsonResponse){
+        ViewSubmissionPayload submissionPayload = GsonFactory.createSnakeCase().fromJson(jsonResponse, ViewSubmissionPayload.class);
 
+        View view = View.builder()
+                .type("modal")
+                .callbackId("create_poll_callback")
+                .title(ViewTitle.builder().type("plain_text").text("Create a poll").build())
+                .submit(ViewSubmit.builder().type("plain_text").text("Select question options").build())
+                .close(ViewClose.builder().type("plain_text").text("Close").build())
+                .notifyOnClose(false)
+                .blocks(submissionPayload.getView().getBlocks())
+                .build();
+
+        ViewSubmissionResponse response = ViewSubmissionResponse.builder().responseAction("update").view(view).build();
+
+        return GsonFactory.createSnakeCase().toJson(response, ViewSubmissionResponse.class);
     }
 }
