c9f7338877a028429b2f7d39eb3ea0cda827ce9f
Aurimas <auruxxas.ar@gmail.com>
2019-11-09 12:54:49 +0200

    no logging folder bug fixed

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -4,15 +4,15 @@ import LoggerLogic.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import sun.rmi.runtime.Log;
 
 import java.io.IOException;
 
 @SpringBootApplication
-@EnableAutoConfiguration
 public class Application {
 
     public static void main(String[] args) throws IOException {
-        Logger logger = new Logger();
         SpringApplication.run(Application.class, args);
+        Logger logger = new Logger();
     }
 }
