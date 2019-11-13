2417dda28ac27831d86268eea8612b949096b3af
Aurimas <auruxxas.ar@gmail.com>
2019-11-13 04:23:52 +0200

    Initial React App With Slack sign in

---
+++ Voutukas/components/PollTable.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class PollTable extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>This is a Poll table</h1>
+      </div>
+    );
+  }
+}
+
+export default PollTable;
