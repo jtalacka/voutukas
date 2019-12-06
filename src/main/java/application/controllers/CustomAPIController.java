f1768321ccd1f088455f9c2a18e08c23f48c873a
Aurimas <auruxxas.ar@gmail.com>
2019-12-07 01:15:54 +0200

    API method to check if user token is valid

--- src/main/java/application/controllers/CustomAPIController.java
+++ src/main/java/application/controllers/CustomAPIController.java
@@ -6,8 +6,14 @@ import application.dto.PollIdDto;
 import application.service.OptionService;
 import application.service.PollService;
 import application.service.UserService;
+import com.github.seratch.jslack.Slack;
+import com.github.seratch.jslack.api.methods.SlackApiException;
+import com.github.seratch.jslack.api.methods.request.auth.AuthTestRequest;
+import com.github.seratch.jslack.api.methods.response.auth.AuthTestResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
@@ -29,5 +35,30 @@ public class CustomAPIController {
     public ResponseEntity<PollResultsDataModel> getPollResultsByPollId(@RequestBody PollIdDto pollID){
         PollResultsDataModel pollResults = pollService.getPollResultsDataById(pollID.getTimeStamp(), pollID.getChannelId());
         return ResponseEntity.ok(pollResults);
+    }
+
+    @GetMapping(value = "/auth/{authId}")
+    public ResponseEntity getIfAuthorised(@PathVariable String authId)
+    {
+        Slack slack = new Slack();
+        try {
+            AuthTestResponse response = slack.methods().authTest(
+              AuthTestRequest.builder().token(authId).build()
+            );
+            System.out.println(response);
+
+            if(response.isOk())
+            {
+                return ResponseEntity.ok().build();
+            }
+            else
+            {
+                ResponseEntity.notFound().build();
+            }
+        } catch (IOException | SlackApiException e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.badRequest().build();
     }
 }
