02a926719a1f2268d3a39098d594a1baf00df7d8
Aurimas <auruxxas.ar@gmail.com>
2019-12-12 01:21:31 +0200

    Functionality: If deleted by API, also deletes from chat

--- src/main/java/application/controllers/PollsAPIController.java
+++ src/main/java/application/controllers/PollsAPIController.java
@@ -3,11 +3,15 @@ package application.controllers;
 import application.dto.PollDto;
 import application.dto.PollIdDto;
 import application.service.PollService;
+import com.github.seratch.jslack.Slack;
+import com.github.seratch.jslack.api.methods.SlackApiException;
+import com.github.seratch.jslack.api.methods.request.chat.ChatDeleteRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -44,6 +48,16 @@ public class PollsAPIController {
     public ResponseEntity deletePollById(@RequestParam String time_stamp, @RequestParam String channel_id)
     {
         pollService.deletePollById(time_stamp, channel_id);
+        Slack slack = new Slack();
+        try {
+            slack.methods(System.getenv("SLACK_API_ACCESS_TOKEN")).chatDelete(ChatDeleteRequest.builder()
+            .channel(channel_id)
+            .ts(time_stamp)
+            .build());
+        } catch (IOException | SlackApiException e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().build();
+        }
         return ResponseEntity.ok().build();
     }
 
